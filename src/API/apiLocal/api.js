1e9353289788549dbaeddbe5585d8362346ba2a8
leo0200 <leonardo2013vivi@hotmail.com>
2023-11-16 16:38:27 -0300

    api_render

--- src/API/apiLocal/api.js
+++ src/API/apiLocal/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiLocal = axios.create({
-    baseURL: 'http://localhost:3333'
+    baseURL: 'https://node-deploy-senac-p0we.onrender.com'
 })
 
 export default apiLocal
